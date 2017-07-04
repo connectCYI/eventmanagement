1087234a5d297def5cc3456832a39569ac0b6c0c
mw509 <emmanuel@eshun-davies.info>
2017-07-04 12:46:23 +0100

    New
    Added Modal Window for Registration

--- index.php
+++ index.php
@@ -2,28 +2,64 @@
 <html>
 <head>
 	<title>CYI Events Portal - Login</title>
-	<link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+
+	<!--CSS Files-->
+	<!--<link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>-->
 	<link rel="stylesheet" type="text/css" href="css/login.css">
+
+	<!-- JS Files -->
+	<script src="js/modal.js"></script>
+
+</head>
+<body>
+	<div class="login_section">
+		<div class = "login_form">
+			<form action="" method="post">
+				<div class="login_header">
+					<img src="css/img/logo.jpg">
+					<h4>CYI Events Portal. Please login to proceed</h4>
+				</div>
+				<input type="text" name="username" placeholder="Username" required>
+				<br/><br/>
+				<input type="password" name="pass" placeholder="Password" required>
+				<br/><br/>
+				<button>LOGIN</button>
+				<br/><br/>
+				<a href="#" id="myBtn">Don't have an account yet? register here</a>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
+
+</style>
 </head>
 <body>
 
-		<div class="login_section">
+<!-- The Modal -->
+<div id="myModal" class="modal">
+	<form action="" method="post">
 
-			<div class = "login_form">
-				<form action="list.php" method="post">
-					<div class="login_header">
-						<img src="css/img/logo.jpg">
-					</div>
-					<input type="text" name="username" placeholder="Username" required>
-					<br/><br/>
-					<input type="password" name="pass" placeholder="Password" required>
-					<br/><br/>
-					<button>LOGIN</button>
-				</form>
-			</div>
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2>New User Registration</h2>
+    </div>
+    <div class="modal-body">
+  		  <input type="text" name="username" placeholder="Username" required>
+  		  <br/><br/>
+  		  <input type="password" name="pass" placeholder="Password" required>
+  		  <br/><br/>
+  		  <button>Register Me</button>
+  		  <br/><br/>
+    </div>
+    <div class="modal-footer">
+      <p>&copy 2017, Christ for Youth International, All Rights Reserved</p>
+    </div>
+  </div>
 
-		</div>
-
-
-</body>
-</html>
+</form>
+</div>
+	<!-- JS Files -->
+	<script src="js/modal.js"></script>
